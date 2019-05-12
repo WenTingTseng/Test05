ade9573f71db6d6d682597cefbd47a351536f20a
fcu-d0381912 <jj1029384756@livemail.tw>
2019-05-12 22:32:52 +0800

    ddd

---
+++ src/main/java/example/Tiger.java
@@ -0,0 +1,10 @@
+package main.java.example;
+
+public class Tiger {
+	public void eat(){
+		System.out.println("Tigger eatting");
+	}
+	public void Sound(){
+		System.out.println("Tigger woo woo woo");
+	}
+}
---
+++ src/test/java/example/TigerTest.java
@@ -0,0 +1,20 @@
+package test.java.example;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import main.java.example.Tiger;
+
+public class TigerTest {
+
+	@Test
+	public void testEat() {
+		new Tiger().eat();
+	}
+	@Test
+	public void testSound() {
+		new Tiger().Sound();
+	}
+
+}
